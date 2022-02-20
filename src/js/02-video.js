e8f22697bd5dde876a67ed96ac2e20098fc9fa7e
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-02-20 19:38:38 +0300

    home work done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(function (event) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(event.seconds));
+  }, 1000),
+  player
+    .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+    .then(function (seconds) {
+      console.log(`Значение времени из localeStorage: ${seconds}`);
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log('Вызвано некорректным временем в setCurrentTime и установило время 0');
+          break;
+
+        default:
+          console.log('ошибки с другими причинами');
+          break;
+      }
+    }),
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import storage from './storage';
+const _ = require('lodash');
 
+const formRef = document.querySelector('form');
+
+if (localStorage.length) {
+  const formInfo = storage.load('feedback-form-state');
+  const localStorageArr = Object.keys(formInfo);
+
+  localStorageArr.forEach(name => {
+    formRef.elements[name].value = formInfo[name];
+  });
+}
+
+formRef.addEventListener('input', _.throttle(updateLocalStorage, 1000));
+formRef.addEventListener('submit', clearFormAndLocalStorage);
+
+function updateLocalStorage() {
+  const feedback = {};
+  const formData = new FormData(formRef);
+  formData.forEach((value, name) => {
+    feedback[name] = value;
+  });
+  storage.save('feedback-form-state', feedback);
+}
+
+function clearFormAndLocalStorage(event) {
+  event.preventDefault();
+  localStorage.clear();
+  formRef.reset();
+}
